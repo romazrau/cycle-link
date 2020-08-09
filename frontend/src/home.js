16b0508b6a0d7c322262b34307e2b8b26d1cc416
a2014946 <68310294+a2014946@users.noreply.github.com>
2020-08-09 19:01:46 +0800

    首頁文字樣板完成
    banner完成
    訊息 完成

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,26 +1,44 @@
-function ClsHome() {
-  var k = 0;
-  var img = new Array();
-  img[0] = "img/A2.jpg";
-  img[1] = "img/A1.jpg";
+let HomeBannerdata =[
+  {
+    fImagePath:"img/A2.jpg",
+    fTitle:"福隆 淨灘活動",
+    fTime:"2 天 08 : 28 : 06",
 
-  function scrollPic() {
-    if (k > img.length - 1) {
-      k = 0;
-    }
-    for (var i = 0; i < img.length; i++) {
-      if (i == k) {
-        document.getElementById("A1").src = img[i];
-      }
-    }
-    k++;
+  },
+  {
+    fImagePath:"img/A1.jpg",
+    fTitle:"小琉球 淨灘活動",
+    fTime:"4 天 01 : 22 : 06",
+
   }
-  setInterval(scrollPic, 5000);
+]
+
+
+function HomeBanner(data) {
+  let postion = 0;
+  function scrollPic() {
+    if (postion > data.length - 1) {
+      postion = 0;
+      
+    }
+        document.getElementById("A1").src = data[postion].fImagePath;
+        document.getElementById("home_top_event_p").innerHTML=data[postion].fTitle+"</br>"+data[postion].fTime;
+    postion++;
+      }
+  setInterval(scrollPic, 2000);
 }
-const Home = new ClsHome();
+
+const Home = new HomeBanner(HomeBannerdata);
+
+document.querySelector(".home_top_event").innerHTML=`<img src="${HomeBannerdata[0].fImagePath}" id="A1">
+<p id="home_top_event_p">${HomeBannerdata[0].fTitle+"</br>"+HomeBannerdata[0].fTime}</p>`
 
 
-const HomePageItemCard =(o)=>{
+
+
+
+
+const HomePage_BuyItemCard =(o)=>{
   
   return `
   <div class="home_market_item_sell">
@@ -63,9 +81,80 @@ let HomePageItemdata = [
 },
 
 ]
-const ItemCard = document.querySelector(".home_market_item");
+
+
+
+const BuyItemCard = document.querySelector(".home_market_item");
+
 HomePageItemdata.map(
   (e, index) => {
-    ItemCard.innerHTML += HomePageItemCard(e);
+    BuyItemCard.innerHTML += HomePage_BuyItemCard(e);
   }
 )
+
+const NewsItemBox=document.querySelector(".home_div_event")
+let HomePageNewsItemdata = [
+  {
+    
+    fTitle: "潛水撿垃圾，愛海洋！",
+    fTime: "2020/07/23 13:00", 
+     fNewsItemLink:"#",
+},
+{
+  
+  fTitle: "淨山撿垃圾，愛地球！",
+  fTime: "2020/08/13 13:00", 
+  fNewsItemLink:"#",
+},
+]
+const HomePage_NewsItem =(o,index)=>{
+ 
+ return `
+ <div class="home_div_event_2">
+  <div class="home_div_event_num"
+      style="border-radius: 5px 5px 0px 0px;margin-bottom: 2px;">
+      <p>${index+1}</p>
+  </div>
+  <div class="home_div_event_text">
+      <a href="${o.fNewsItemLink}">${o.fTitle} 於 ${o.fTime} 開始</a>
+  </div>
+</div>`
+}
+
+
+HomePageNewsItemdata.map(
+  (e, index) => 
+  {
+  
+    NewsItemBox.innerHTML +=HomePage_NewsItem(e,index);
+  }
+)
+  
+
+
+const NewsRightBox=document.querySelector(".home_div_news")
+
+const HomePage_NewsRightBox= (o)=>{
+  return `<div>
+  <img src="img/qk1.png" class="home_icon" alt="">
+  <h3>${o.fTitle}</h3>
+  
+  <img src="./img/qk2.png" class="home_icon" alt="">
+</div>
+<img src="${o.fImagePath}" class="home_div_news_img" alt="">
+<p>${o.fNewsContent}
+</p>
+<a href="${o.fNewsLink}">See more...</a>   `
+
+}
+
+let NewsRightData=[{
+  fTitle: "有機咖啡豆",
+  fImagePath: "img/coffee.jpg", 
+  fNewsContent:"咖啡越新鮮越好…對吧？這個說法其實不完全對。雖然沒有人想喝過期的老豆，但烘焙好的咖啡豆直接沖煮，其風味一定會讓你失望，因為咖啡這時仍在大量排氣的狀態。不論你是烘焙者還是沖煮者，你都必須了解排氣，本文讓我們了解什麼是排氣（養豆），排氣如何影響你沖的咖啡、以及要如何進行這些動作。",
+  fNewsLink:"#",
+},]
+NewsRightData.map((e,index) =>{
+  NewsRightBox.innerHTML+=HomePage_NewsRightBox(e);
+})
+
