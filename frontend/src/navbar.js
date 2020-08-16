ddf7a61bd2f80e34fbef1648e9fa418ed4a51347
romazrau <b04208017@ntu.edu.tw>
2020-08-16 23:50:18 +0800

    feat: navbar的gif 會動了

--- frontend/src/navbar.js
+++ frontend/src/navbar.js
@@ -1,19 +1,48 @@
-function ClsNavbar(){
-    let pageY =  window.pageYOffset;
+function ClsNavbar() {
+    let pageY = window.pageYOffset;
     const navbarMain = document.querySelector("#navbar_main");
     window.addEventListener(
-        'scroll', 
-        () =>{
-            if( window.pageYOffset - pageY > 0){
+        'scroll',
+        () => {
+            if (window.pageYOffset - pageY > 0) {
                 navbarMain.classList.add("transYsWidthTo0");
-            }else{
+            } else {
                 navbarMain.classList.remove("transYsWidthTo0");
-    
+
             }
-    
-            pageY =  window.pageYOffset;
+
+            pageY = window.pageYOffset;
         }
     );
+
+
+    //title img
+    let imGcount = 1;
+    let isGIFrun = 0;
+    document.querySelector(".header_title").addEventListener(
+        "mouseenter",
+        (e) => {
+            if (!isGIFrun) {
+                new Promise(
+                    (res, rej) => {
+                        isGIFrun = 1;
+                        e.currentTarget.innerHTML = "";
+                        const gif = document.createElement("img");
+                        gif.src = "./img/ 海龜gogoGIF.gif?" + imGcount;
+                        gif.alt = "海龜";
+                        e.currentTarget.appendChild(gif);
+                        imGcount++;
+                        setTimeout(
+                            ()=>{res("OK")},
+                            1500
+                        )
+                    }
+                ).then(()=>{
+                    isGIFrun = 0;
+                })
+            }
+        }
+    )
 
 
 
