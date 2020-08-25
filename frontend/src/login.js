8ef844af688904cd8affba75ef5a9ebc73223f7c
a2014946 <68310294+a2014946@users.noreply.github.com>
2020-08-25 16:32:55 +0800

    登入跳轉會員頁面

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,43 +1,43 @@
-// import {
-//     serverURL
-// } from "./api.js"
+import {
+    serverURL
+} from "./api.js"
 
 
-// document.querySelector("#loginSubmit").addEventListener(
-//     "click",
-//     (e) => {
-//         e.preventDefault();
-//         // let form = document.querySelector( ".form_signIn" ); 
-//         // let formData = new FormData(form);
+document.querySelector("#loginSubmit").addEventListener(
+    "click",
+    (e) => {
+        e.preventDefault();
+        // let form = document.querySelector( ".form_signIn" ); 
+        // let formData = new FormData(form);
 
-//         // fetch( serverURL.login ,{
-//         //     method: "POST",
-//         //     body: formData,
-//         //     credentials: 'include'
-//         // }).then(
-//         //     (res) => res.text()
-//         // ).then(
-//         //     resStr => {
-//         //     const resData = JSON.parse(resStr);
-//         //     console.log(resData);
-//         //     if(resData.result === "1"){
-//         //         let show = `${resData.name}<br/>歡迎`;
-//         //         document.querySelector("#header_link_login").innerHTML = show;
-//         //     }
-//         // }
+        // fetch( serverURL.login ,{
+        //     method: "POST",
+        //     body: formData,
+        //     credentials: 'include'
+        // }).then(
+        //     (res) => res.text()
+        // ).then(
+        //     resStr => {
+        //     const resData = JSON.parse(resStr);
+        //     console.log(resData);
+        //     if(resData.result === "1"){
+        //         let show = `${resData.name}<br/>歡迎`;
+        //         document.querySelector("#header_link_login").innerHTML = show;
+        //     }
+        // }
 
-//         // ).catch(
-//         //     err => console.log(err)
-//         // )
-
-
-//         let show = `<i class="fas fa-bullhorn login_bullhorn"></i> <div><p>哈哈松鼠</p><p>歡迎</p></div>`;
-//         document.querySelector("#header_link_login").innerHTML = show;
-//         location.hash = "#personal-maneger";
+        // ).catch(
+        //     err => console.log(err)
+        // )
 
 
-//     }
-// )
+        let show = `<i class="fas fa-bullhorn login_bullhorn"></i> <div><p>哈哈松鼠</p><p>歡迎</p></div>`;
+        document.querySelector("#header_link_login").innerHTML = show;
+        location.hash = "#personal-maneger";
+
+
+    }
+)
 
 
 const signUpButton = document.getElementById('Login_signUp');
