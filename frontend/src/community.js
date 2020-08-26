d5d24645b5b6c2745bf5c61ea445f8f5d42e6d99
JIH-L <sol@livemail.tw>
2020-08-26 14:51:54 +0800

    修正: 社團第二條navbar

--- frontend/src/community.js
+++ frontend/src/community.js
@@ -1,32 +1,32 @@
-   function ClsCommunity(){
+   function ClsCommunity() {
 
-   // *second nav bar
-   let pageY = window.pageYOffset;
-   const communityNavbar = document.querySelector("#community_navbar");
-   window.addEventListener(
-       'scroll',
-       () => {
-           if (window.pageYOffset - pageY > 0) {
-               communityNavbar.classList.remove("transYsWidthTo0Bottom");
-           } else {
-               communityNavbar.classList.add("transYsWidthTo0Bottom");
+       // *second nav bar
+       //    let pageY = window.pageYOffset;
+       //    const communityNavbar = document.querySelector("#community_navbar");
+       //    window.addEventListener(
+       //        'scroll',
+       //        () => {
+       //            if (window.pageYOffset - pageY > 0) {
+       //                communityNavbar.classList.remove("transYsWidthTo0Bottom");
+       //            } else {
+       //                communityNavbar.classList.add("transYsWidthTo0Bottom");
+       //            }
+       //            pageY = window.pageYOffset;
+       //        }
+       //    );
+
+
+       //跳轉
+       document.querySelectorAll(".communmity_container_middle_content ").forEach(
+           (item) => {
+               item.addEventListener(
+                   "click",
+                   (e) => {
+                       location.hash = "#community/detail/id=?";
+                   }
+               )
            }
-           pageY = window.pageYOffset;
-       }
-   );
-
-
-   //跳轉
-   document.querySelectorAll(".communmity_container_middle_content ").forEach(
-       (item) => {
-           item.addEventListener(
-               "click",
-               (e) => {
-                   location.hash = "#community/detail/id=?";
-               }
-           )
-       }
-   )
+       )
 
 
 
@@ -34,13 +34,9 @@
 
 
 
-}
-   
-   
-   
-
-const Community = new ClsCommunity();
+   }
 
 
 
-    
+
+   const Community = new ClsCommunity();
