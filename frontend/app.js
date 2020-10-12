fba1332b20b574c1fe0af5d23f0a3ba024f3ab4d
romazrau <b04208017@ntu.edu.tw>
2020-10-12 17:47:51 +0800

    frontend: 伺服器建設

---
+++ frontend/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+const open = require('open');
+
+
+app.use(express.static('public'));
+
+
+app.get("/", function (req, res) {
+  res.send('./public/index.htmls');
+})
+
+
+app.use('/', function (req, res) {
+  res.redirect('/');
+});
+
+
+
+app.listen(5502, function () {
+  console.log('Example app listening on port 5502!');
+  (async () => {
+    // Opens the URL in the default browser.
+    await open("http://localhost:5502");
+  })();
+});
